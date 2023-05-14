21094bc2d50ee5fdfb9c29b1fe6d795787898108
GerardoRodr <gerardorodriguezmonzon@gmail.com>
2023-05-13 23:19:52 -0500

    Inicio del proyecto

---
+++ src/test/java/com/cibertec/BurgerBrossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerBrossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
